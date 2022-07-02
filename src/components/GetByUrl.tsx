86c054e08dd73a1e5110f195b4ed4bb8733b08f3
tahmdev <qwertiimusic@gmail.com>
2022-07-02 15:37:50 +0200

    Improve UX

--- src/components/GetByUrl.tsx
+++ src/components/GetByUrl.tsx
@@ -1,8 +1,9 @@
 import React, {
   ChangeEventHandler,
-  Dispatch,
-  SetStateAction,
   useContext,
+  useEffect,
+  useLayoutEffect,
+  useRef,
   useState,
 } from "react";
 import { ArticleContext } from "../context/ArticleCTX";
@@ -11,6 +12,9 @@ interface Props {
   hide: () => void;
 }
 export const GetByURL: React.FC<Props> = ({ hide }) => {
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const initialRender = useRef(true);
   const [queryUrl, setQueryUrl] = useState("");
   const { setArticle } = useContext(ArticleContext);
 
@@ -25,9 +29,35 @@ export const GetByURL: React.FC<Props> = ({ hide }) => {
     hide();
   };
 
+  useLayoutEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  const handleClick = (e: MouseEvent) => {
+    if (
+      e.target instanceof Node &&
+      !wrapperRef.current?.contains(e.target) &&
+      !initialRender.current
+    ) {
+      hide();
+    } else {
+      initialRender.current = false;
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, [handleClick]);
+
   return (
-    <div className="get-by-url">
-      <input value={queryUrl} onChange={handleChange} placeholder="url" />
+    <div className="get-by-url" ref={wrapperRef}>
+      <input
+        value={queryUrl}
+        onChange={handleChange}
+        placeholder="url"
+        ref={inputRef}
+      />
       <button onClick={handleQuery}>Get</button>
     </div>
   );
